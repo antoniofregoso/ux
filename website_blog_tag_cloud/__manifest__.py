2b82505da16c81d154a752adbff5fefeba552289
antoniofregoso <antoniofregoso@antoniofregoso-home>
2019-12-16 00:10:16 -0600

    [ADD] Tag Cloud module alpha

---
+++ website_blog_tag_cloud/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Blog Tag Cloud",
+
+    'summary': "Generate a tag cloud that is activated with a floating button",
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Website/Website',
+    'version': '13.0.0.0.0',
+
+    'depends': ['website_blog'],
+
+    'data': [
+        'views/website_blog_tag_cloud_templates.xml',
+    ],
+
+    
+}
