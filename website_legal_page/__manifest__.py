ec87c9764911e27fb9a10789600acc8a6ebf2f0b
antoniofregoso <antoniofregoso@antoniofregoso-home>
2020-01-11 23:13:25 -0600

    [ADD] Legal_page module

---
+++ website_legal_page/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2015 Antonio Espinosa <antonioea@antiun.com>
+# Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+{
+    'name': "Website Legal Page",
+
+
+    'author': "Antonio Fregoso",
+    'website': "https://antoniofregoso.com",
+
+
+    'category': 'Website/Website',
+    'version': '13.0.0.0.0',
+
+    'depends': ['website'],
+
+
+    'data': [
+        'views/reusable_templates.xml',
+        'views/website_legal.xml',
+        'views/website_privacy.xml',
+        'views/website_terms.xml',
+    ],
+    'license': 'AGPL-3',
+
+}
