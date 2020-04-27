06ac4c75fd1923f2c9ae5d41359874c1e64d709a
antoniofregoso <antonio.fregoso.mx@gmail.com>
2020-04-27 14:40:26 -0500

    [FIX] Maps

--- website_google_maps_styling/controllers/main.py
+++ website_google_maps_styling/controllers/main.py
@@ -12,33 +12,6 @@ _logger = logging.getLogger(__name__)
 
 class GoogleMap(http.Controller):
     
-    @http.route(['/google_map_theme'], type='json', auth="public", website=True, sitemap=False)
-    def google_map_theme(self, template):
-        theme = request.website.google_map_theme_id
-        js_theme = []
-        if theme:   
-            if theme.geometry_visibility:
-                js_theme.append({"elementType": "geometry","stylers":[{ "color":theme.geometry_color}]})
-            else:
-                js_theme.append({"elementType": "geometry","stylers":[{ "visibility":"off"}]})
-            if not theme.labels_icon:
-                js_theme.append({"elementType": "labels.icon","stylers":[{ "visibility":"off"}]})
-            if theme.text_stroke_visibility:
-                js_theme.append({"elementType": "labels.text.stroke","stylers":[{ "color":theme.text_stroke_color}]}) 
-            else:
-                js_theme.append({"elementType": "labels.text.stroke","stylers":[{ "visibility":"off"}]})  
-            if theme.text_fill_visibility:
-                js_theme.append({"elementType": "labels.text.fill","stylers":[{ "color":theme.text_fill_color}]})
-            else:
-                js_theme.append({"elementType": "labels.text.fill","stylers":[{ "visibility":"off"}]})  
-            for option in theme.options_ids:
-                js_option = {"featureType": option.feature, "elementType": option.element , "stylers":[]}
-                if option.visibility:
-                    js_option["stylers"]= [{"color":option.color}]
-                else:
-                    js_option["stylers"]= [{"visibility": "off"}] 
-                js_theme.append(js_option)         
-            return request.website.viewref(template).render({'theme': js_theme})
         
     @http.route(['/map'], type='http', auth="public", website=True, sitemap=False)
     def google_map(self,company, **kw):
