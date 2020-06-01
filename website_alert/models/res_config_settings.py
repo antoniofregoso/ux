219bc4eef9df73532656876b044d928b7c413fed
antoniofregoso <antonio.fregoso.mx@gmail.com>
2020-06-01 17:24:13 -0500

    [ADD] Add website alert module

--- website_alert/models/res_config_settings.py
+++ website_alert/models/res_config_settings.py
@@ -7,3 +7,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     alert_color = fields.Char('Background Color',related='website_id.alert_color', readonly=False)
+    alert_message_color = fields.Char(string="Message Color", related='website_id.alert_message_color', readonly=False)
+    alert_title =  fields.Char('Title',related='website_id.alert_title', readonly=False)
+    alert_message1 = fields.Char('Message 1',related='website_id.alert_message1', readonly=False)
+    alert_message2 = fields.Char('message 2',related='website_id.alert_message2', readonly=False)
--- website_alert/models/website.py
+++ website_alert/models/website.py
@@ -8,6 +8,10 @@ class Website(models.Model):
     _inherit = 'website'
     
     alert_color = fields.Char(string="Background Color", help="Choose your color", default="#ff0000")
+    alert_message_color = fields.Char(string="Message Color", help="Choose your color", default="#ffffff")
+    alert_title =  fields.Char('Title', default="Important Message")
+    alert_message1 = fields.Char('Message 1', default="Aww yeah, you successfully read this important alert message")
+    alert_message2 = fields.Char('message 2', default="Aww yeah, you successfully read this important alert message")
     
     
     
