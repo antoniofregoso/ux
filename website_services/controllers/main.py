0f379e2894228e2c32173309a689454ebaa556ab
antoniofregoso <antonio.fregoso.mx@gmail.com>
2020-06-02 13:11:34 -0500

    [TEst]

--- website_services/controllers/main.py
+++ website_services/controllers/main.py
@@ -10,6 +10,6 @@ class WebsiteServices(http.Controller):
     
     @http.route(['/services/get_services'], type='json', auth='public', methods=['POST'], website=True)
     def get_services(self, template):
-        _logger.warn('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww: {}'.format([('landig_page_published', '=', True), ('sale_ok', '=', True), '|', ('website_id','=', request.website.id), ('website_id','=', False)]))
         services = request.env['product.template'].search([('landig_page_published', '=', True), ('sale_ok', '=', True), '|', ('website_id','=', request.website.id), ('website_id','=', False)], order="sequence" )
+        _logger.warn('ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc: {}:'.format(len(services)))
         return request.website.viewref(template).render({'services': services})
