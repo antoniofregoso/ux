0f0ea760d7414aac4249eae74e54abaed6dacd58
antoniofregoso <antonio.fregoso.mx@gmail.com>
2020-06-02 12:53:53 -0500

    [TEST] error log

--- website_services/controllers/main.py
+++ website_services/controllers/main.py
@@ -10,5 +10,6 @@ class WebsiteServices(http.Controller):
     
     @http.route(['/services/get_services'], type='json', auth='public', methods=['POST'], website=True)
     def get_services(self, template):
+        _logger.warn('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww: {}'.format([('landig_page_published', '=', True), ('sale_ok', '=', True), '|', ('website_id','=', request.website.id), ('website_id','=', False)]))
         services = request.env['product.template'].search([('landig_page_published', '=', True), ('sale_ok', '=', True), '|', ('website_id','=', request.website.id), ('website_id','=', False)], order="sequence" )
         return request.website.viewref(template).render({'services': services})
