e91d3cd41b7d4011094adedc3d54ba2c0a028f76
antoniofregoso <antonio.fregoso.mx@gmail.com>
2020-07-09 11:26:09 -0500

    [TEST]

--- website_services/controllers/main.py
+++ website_services/controllers/main.py
@@ -3,6 +3,9 @@
 from odoo import http
 from odoo.http import request
 
+import logging
+_logger = logging.getLogger(__name__)
+
 
 
 class WebsiteServices(http.Controller):
@@ -10,5 +13,5 @@ class WebsiteServices(http.Controller):
     @http.route(['/services/get_services'], type='json', auth='public', methods=['POST'], website=True)
     def get_services(self, template):
         services = request.env['product.template'].sudo().search([('landig_page_published', '=', True), ('sale_ok', '=', True), '|', ('website_id','=', request.website.id), ('website_id','=', False)], order="sequence" )
-
+        _logger.warn('WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWw: {}'.format(services))
         return request.website.viewref(template).render({'services': services})
