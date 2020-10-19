015f32e981a95c2bc9cbf04a7a4b28e09e188666
antoniofregoso <yomero@antoniofregoso.com>
2020-10-18 20:27:31 -0500

    [ADD] Brand

---
+++ website_branding/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).#
+{
+    "name": "Change brand message",
+    "version": "1.0.0",
+    "author": "Antonio Fregoso",
+    "website": "https:www.antoniofregoso",
+    "license": "LGPL-3",
+    "category": "Website",
+    "depends": ["website"],
+    "data": ["templates/disable_odoo.xml"],
+    "installable": True,
+}
