6ede82c2c8499748c22b25271623915b9cf8a89c
antoniofregoso <yomero@antoniofregoso.com>
2020-11-04 18:01:16 -0600

    [fix]

--- website_services/controllers/main.py
+++ website_services/controllers/main.py
@@ -6,7 +6,7 @@ from odoo.http import request
 class WebsiteServices(http.Controller):
 
     
-    @http.route(['/services/get_services'], type='json', auth='public', methods=['POST'], website=True)
+    @http.route(['/services/get_services'], type='json', auth='public', website=True)
     def get_services(self, template):
         domain = [('landig_page_published', '=', True), ('sale_ok', '=', True), '|', ('website_id','=', request.website.id), ('website_id','=', False)]
         services = request.env['product.template'].search(domain, order="sequence" )
