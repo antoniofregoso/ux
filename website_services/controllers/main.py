45959b43aa7657396d371938a74429d2644d4170
antoniofregoso <yomero@antoniofregoso.com>
2020-11-04 18:36:43 -0600

    [FIX]

--- website_services/controllers/main.py
+++ website_services/controllers/main.py
@@ -9,7 +9,7 @@ class WebsiteServices(http.Controller):
     @http.route(['/services/get_services'], type='json', auth='public', website=True)
     def get_services(self, template):
         domain = [('landig_page_published', '=', True), ('sale_ok', '=', True), '|', ('website_id','=', request.website.id), ('website_id','=', False)]
-        services = request.env['product.template'].search(domain, order="sequence" )
+        services = request.env['product.template'].sudo().search(domain, order="sequence" )
         values = {'suscribe':_('Subscribe'),'apply':_('Apply'),'reserve':_('Reserve'),'download':_('Download'),'get_offer':_('Get Offer'),'quote':_('Quote'),'sign_up':_('Sign Up'),'more_info':_('More Information')}
         return request.website.viewref(template)._render({'services': services, 'call_to_action':values})
 
