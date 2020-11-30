43c7dbf62a3e4375f2abfc946e510ed831fb445c
antoniofregoso <yomero@antoniofregoso.com>
2020-11-29 23:17:02 -0600

    [FIX] Animation Options

--- website_animated/static/src/js/animated.editor.js
+++ website_animated/static/src/js/animated.editor.js
@@ -1,39 +1,25 @@
-odoo.define('website_animations.snippets.options', function (require) {
+odoo.define('website_animations.editor', function (require) {
     'use strict';
     
     var core = require('web.core');
-    var sOptions = require('web_editor.snippets.options');
-    var wUtils = require('website.utils');
+    const options = require('web_editor.snippets.options');
     
-    var _t = core._t;
-    var qweb = core.qweb;
-    
-    sOptions.registry.js_set_animated = sOptions.Class.extend({
+    options.registry.AnimatedCSS = options.Class.extend({
         
          /**
          * @see this.selectClass for parameters
          */
     
         
+        _setOptionsDefaultValues: function () {},
+
         _onLinkClick: function (ev) {
-             var option = $(ev.target).closest('we-button').attr('data-animated');
-                 this.$el.find('[data-animated="' + this.animation + '"]').removeClass('active');
-                 this.$target.find('[data-animated]').attr('data-animated', option);
-                 this.$el.find('[data-animated="' + option + '"]').addClass('active');
-                 this.animation = option;
+            console.log('Hola');
         },
         
+            
         
-         _setActive: function () {
-             this._super.apply(this, arguments);	
-             this.animation =  this.$target.find('[data-animated]').data('animated');
-             if (this.animation== undefined){
-                 this.animation = 'none';
-                var w = this.$target.prop('data-animated', 'Puto');
-                 alert(w.html());
-             }
-             this.$el.find('[data-animated="' + this.animation + '"]').addClass('active');
-         },
+        
         
     });
     
